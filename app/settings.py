91bab71e4367ca74163d72471db8052d3ad14da9
vladyslavgolovanov <cuzuki1@gmail.com>
2020-12-27 16:46:43 +0200

    'postgresql'

--- app/settings.py
+++ app/settings.py
@@ -78,10 +78,22 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'user',
+        'PASSWORD': 'pass',
+        'HOST': '*',
+        'PORT': '5432'
+
+
     }
 }
 
